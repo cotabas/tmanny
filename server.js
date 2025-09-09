a2c2515225d0f86cc113042b01cd28c840d37707
cotabas <cotabas@gmail.com>
2025-09-09 14:33:41 -0700

    readme

--- server.js
+++ server.js
@@ -26,5 +26,9 @@ app.get('/api/test-db', async (req, res) => {
   }
 });
 
+// Routes
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
